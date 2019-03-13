14032c85fb6e4cb1a810e0f6c04763dea4c1258d
lokraszewski <lokraszewski@innovative-technology.com>
2019-03-13 11:35:11 +0000

    Code comment + arr size calc

--- c/permute.c
+++ c/permute.c
@@ -18,6 +18,7 @@ void permute(int* arr, int st, int ls)
 {
   if (st == ls)
   {
+    // We have reached a possible permutation, print it for the user.
     print_arr(arr, ls);
   }
   else
@@ -25,7 +26,9 @@ void permute(int* arr, int st, int ls)
     for (int i = st; i < ls; i++)
     {
       swap(arr + st, arr + i);
+      // Call this recursively skipping the start element. Eventually we will reach the termination condition (st == ls)
       permute(arr, st + 1, ls);
+      // Swap back to restore the array to original version.
       swap(arr + st, arr + i);
     }
   }
@@ -33,8 +36,11 @@ void permute(int* arr, int st, int ls)
 
 int main(int argc, char const* argv[])
 {
-  int arr[] = {0, 1, 2, 3};
-  permute(arr, 0, 4);
+  int          arr[]    = {0, 1, 2, 3};
+  const size_t arr_size = sizeof(arr) / sizeof(arr[0]);
+  permute(arr, 0, arr_size);
+  printf("Original: ");
+  print_arr(arr, arr_size);
 
   return 0;
 }
